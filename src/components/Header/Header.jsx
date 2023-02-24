21486993873066f6ca8926c8a1268fea16d8ecaf
”rsshonjoydas” <rsshonjoydas@email.com>
2023-02-24 14:36:57 +0600

    header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import { motion } from "framer-motion";
+import React, { useRef, useState } from "react";
+import { BiMenuAltRight, BiPhoneCall } from "react-icons/bi";
+import useHeaderShadow from "../../hooks/useHeaderShadow";
+import useOutsideAlerter from "../../hooks/useOutsideAlerter";
+import { getMenuStyles, headerVariants } from "../../utils/motion";
+import css from "./Header.module.scss";
+
+const Header = () => {
+  const menuRef = useRef(null);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerShadow = useHeaderShadow();
+
+  //to handle click outside of sidebar on mobile
+  useOutsideAlerter({
+    menuRef,
+    setMenuOpened,
+  });
+
+  return (
+    <motion.div
+      variants={headerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`bg-primary paddings ${css.wrapper}`}
+      viewport={{ once: true, amount: 0.25 }}
+      style={{boxShadow: headerShadow}}
+    >
+      <div className={`innerWidth ${css.container} flexCenter`}>
+        <div className={css.name}>Shonjoy</div>
+        <ul
+          className={`flexCenter ${css.menu}`}
+          ref={menuRef}
+          style={getMenuStyles(menuOpened)}
+        >
+          <li><a href="#service">Services</a></li>
+          <li><a href="#work">Experience</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#people">Testimonials</a></li>
+          <li className={`flexCenter ${css.phone}`}>
+            <p>+880 1311 401 701</p>
+            <BiPhoneCall size={"40px"} />
+          </li>
+        </ul>
+
+        {/* for medium and small screens */}
+        <div
+          className={css.menuIcon}
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Header;
